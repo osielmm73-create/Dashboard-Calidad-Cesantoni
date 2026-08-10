082737105955f130e09620b2c48e0ba26e78258c
osielmm73-create <osielmm73@gmail.com>
2026-08-10 16:27:06 -0600

    app.py

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
-st.set_page_config(
-    page_title="Dashboard Calidad",
-    layout="wide"
-)
+st.title("Explorador de Excel")
 
-st.title("📊 Dashboard de Calidad")
-
-archivo = st.file_uploader(
-    "Arrastra tu archivo Excel aquí",
-    type=["xlsx"]
-)
+archivo = st.file_uploader("Sube el Excel", type=["xlsx"])
 
 if archivo:
 
-    try:
-        excel = pd.ExcelFile(archivo)
+    excel = pd.ExcelFile(archivo)
 
-        st.write("Hojas encontradas:")
-        st.write(excel.sheet_names)
+    hoja = st.selectbox(
+        "Hoja",
+        excel.sheet_names
+    )
 
-        hoja = excel.sheet_names[0]
+    df = pd.read_excel(
+        archivo,
+        sheet_name=hoja,
+        header=None
+    )
 
-        df = pd.read_excel(
-            archivo,
-            sheet_name=hoja
-        )
-
-        st.success(f"Hoja cargada: {hoja}")
-
-        st.write("Columnas detectadas:")
-        st.write(df.columns.tolist())
-
-        st.write(df.head())
-
-    except Exception as e:
-        st.error(f"Error al leer archivo: {e}")
+    st.write(df.head(50))
